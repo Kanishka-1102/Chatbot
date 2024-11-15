6902cafbad0474e59b65c6068292424f0b86c14f
Kanishka-1102 <kanishkamps11c@gmail.com>
2024-11-15 17:46:34 +0530

    add

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
+import streamlit as st
+from dotenv import load_dotenv
+
+
 
 def main():
-    print("Hello World")
+    load_dotenv()
+    st.set_page_config(page_title="Vedabot",page_icon=":books:")
+    st.header("Chat with Vedabot :books:")
+
+    st.text_input("Ask A Question About health or Problem :")
+
+    
 
 if __name__=='__main__':
     main()
